46b3c82d471b34208fce419f3d92e5243f415634
Alma Arevalo <olaverax@gmail.com>
2026-03-25 20:08:32 -0400

    Fix test_solver

--- tests/test_solver.py
+++ tests/test_solver.py
@@ -2,7 +2,7 @@ from solver import solve_cvrp
 from utils import load_data
 
 def test_solver_runs():
-    data = load_data("data/instance.json")
+    data = load_data("data/cvrp_problem_data.json")
     solution = solve_cvrp(data)
 
     assert solution["success"] is True
