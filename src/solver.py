66c86fc23778c8eecf9dee4560b23b869a841b1b
Alma Arevalo <olaverax@gmail.com>
2026-03-25 23:25:54 -0400

    Add routing methods and testing

--- src/solver.py
+++ src/solver.py
@@ -3,9 +3,67 @@ from typing import Dict, Any
 
 
 CVRPInput = Dict[str, Any]
+CVRPModel = Dict[str, Any]
 CVRPOutput = Dict[str, Any]
 
+
+# Define default variables
+num_vehicles = 2
+num_nodes = 21
+depot = 0
+
+# Create manager
+def create_manager(num_nodes, num_vehicles, depot):
+    return pywrapcp.RoutingIndexManager(num_nodes, num_vehicles, depot)
+    
+# Create routing model
+def create_routing_model(manager):
+    return pywrapcp.RoutingModel(manager)
+    
+    
+# Convert from routing variable Index to distance matrix NodeIndex.
+def distance_callback(from_index, to_index, manager, distance_matrix):
+    """
+    Returns the distance between the two nodes.
+    """
+    from_node = manager.IndexToNode(from_index)
+    to_node = manager.IndexToNode(to_index)
+    return distance_matrix[from_node][to_node]
+
+#Assign the cost of every arc equal to the distance for all vehicles
+  
+def cost_evaluator(routing, distance_callback):
+    transit_callback_index = routing.RegisterTransitCallback(distance_callback)
+    routing.SetArcCostEvaluatorOfAllVehicles(transit_callback_index)
+    return transit_callback_index
+
+
+
 def solve_cvrp(data: CVRPInput) -> CVRPOutput:
+    """
+    Solver with OR-tools
+    """
+    
+    # Simple dummy solution: assign each node to first vehicle in order
+    routes = [[0] + list(range(1, num_nodes)) + [0]]  # single route covering all
+    while len(routes) < num_vehicles:
+        routes.append([0, 0])  # empty routes for extra vehicles
+    vehicle_distances = [1000]
+    while len(routes) < num_vehicles:
+        vehicle_distances.append([0, 0])  # distance 0 for extra vehicles
+
+    total_distance = sum(vehicle_distances) 
+    vehicle_loads = [sum(data["demands"])] + [0]*(num_vehicles-1)
+
+    return {
+        "routes": routes,
+        "vehicle_distances": vehicle_distances,
+        "total_distance": total_distance,
+        "vehicle_loads": vehicle_loads,
+        "success": True
+    }
+
+def fake_solve_cvrp(data: CVRPInput) -> CVRPOutput:
     """
     Black-box placeholder for CVRP solver.
     Currently just returns a dummy solution.
--- tests/test_solver.py
+++ tests/test_solver.py
@@ -1,4 +1,6 @@
-from solver import solve_cvrp
+import unittest
+from ortools.constraint_solver import pywrapcp
+from solver import solve_cvrp, create_manager, create_routing_model, distance_callback, cost_evaluator
 from utils import load_data
 
 def test_solver_runs():
@@ -7,3 +9,49 @@ def test_solver_runs():
 
     assert solution["success"] is True
     assert isinstance(solution["routes"], list)
+
+class TestRoutingFunctions(unittest.TestCase):
+
+    def setUp(self):
+        # Example distance matrix
+        self.distance_matrix = [
+            [0, 10, 15],
+            [10, 0, 20],
+            [15, 20, 0]
+        ]
+        self.num_nodes = len(self.distance_matrix)
+        self.num_vehicles = 1
+        self.depot = 0
+
+        # Create manager and routing model
+        self.manager = create_manager(self.num_nodes, self.num_vehicles, self.depot)
+        self.routing = create_routing_model(self.manager)
+
+    def test_create_manager(self):
+        self.assertEqual(self.manager.GetNumberOfNodes(), self.num_nodes)
+        self.assertEqual(self.manager.GetNumberOfVehicles(), self.num_vehicles)
+        #self.assertEqual(self.manager.Start(0), self.depot)
+        # Check start and end indices for all vehicles
+        # starting_at_depot = all(self.manager.Start(v) == self.depot for v in range(self.num_vehicles))
+        # ending_at_depot   = all(self.manager.End(v) == self.depot for v in range(self.num_vehicles))
+        # self.assertTrue(starting_at_depot)
+        # self.assertTrue(ending_at_depot)
+
+    def test_create_routing_model(self):
+        self.assertIsInstance(self.routing, pywrapcp.RoutingModel)
+
+    def test_distance_callback(self):
+        # test some known distances
+        self.assertEqual(distance_callback(0, 1, self.manager, self.distance_matrix), 10)
+        self.assertEqual(distance_callback(1, 2, self.manager, self.distance_matrix), 20)
+        self.assertEqual(distance_callback(2, 0, self.manager, self.distance_matrix), 15)
+
+    def test_cost_evaluator(self):
+        def callback(from_index, to_index):
+            return distance_callback(from_index, to_index, self.manager, self.distance_matrix)
+
+        transit_index = cost_evaluator(self.routing, callback)
+        self.assertIsInstance(transit_index, int)  # should return a callback index
+
+if __name__ == "__main__":
+    unittest.main()
