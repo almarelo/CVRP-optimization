e2faeca86e82e7714c883a49192722a0eef3990b
Alma Arevalo <olaverax@gmail.com>
2026-03-26 12:48:00 -0400

    Add function to verify the solution is compliant to the constraints

--- src/utils.py
+++ src/utils.py
@@ -46,3 +46,54 @@ def load_data(path: str):
 
     
     return data
+
+
+    
+
+def verify_constraints(solution, data):
+    if data["constraints"]["each_node_visited_once"]:
+        visited_nodes = []
+        for route in solution["routes"]:
+            for node in route:
+                if node != 0:
+                    visited_nodes.append(node)
+        if sorted(visited_nodes) != sorted(data["nodes"]["delivery_locations"]):
+            raise ValueError("Not all nodes were visited")
+    
+    if data["constraints"]["vehicles_start_and_end_at_depot"]:
+        for route in solution["routes"]:
+            if route[0] != 0 or route[-1] != 0:
+                raise ValueError("Not all vehicles start and end at depot")
+    
+    if data["constraints"]["vehicle_capacity_respected"]:
+        for route in solution["routes"]:
+            if sum(data["demands"][node] for node in route) > data["vehicles"]["capacity_per_vehicle"]:
+                raise ValueError("Not all vehicles capacity respected")
+    
+    return True
+
+
+def print_solution(solution, data):
+    print(f"Total distance: {solution['total_distance']}")
+    for vehicle_id in range(data["vehicles"]["count"]):
+        print(f"Route for vehicle {vehicle_id}:")
+        index = routing.Start(vehicle_id)
+        plan_output = ""
+        route_distance = 0
+        route_load = 0
+        while not routing.IsEnd(index):
+            node_index = manager.IndexToNode(index)
+            route_load += data["demands"][node_index]
+            plan_output += f" {node_index} ->"
+            previous_index = index
+            index = solution.Value(routing.NextVar(index))
+            route_distance += routing.GetArcCostForVehicle(
+                previous_index, index, vehicle_id
+            )
+        node_index = manager.IndexToNode(index)
+        plan_output += f" {node_index}"
+        plan_output += f"\nLoad of the route: {route_load}"
+        plan_output += f"\nDistance of the route: {route_distance}"
+        print(plan_output)
+    
+    
